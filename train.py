d19943ef0fbdff0d3e17d65893acd389d0360f22
Thiago Ambiel <thiago.ambiel1@gmail.com>
2022-06-12 18:09:57 -0300

    Add training arguments

--- train.py
+++ train.py
@@ -19,6 +19,14 @@ if __name__ == "__main__":
                         help="Number of steps to pretrain the foreground "
                              "and background models. (Default: 100)")
 
+    parser.add_argument("-bs", "--bootstrapping_steps", type=int, default=10_000,
+                        help="Number of steps to train the alpha model with"
+                             "supervised learning. (Default: 10_000)")
+
+    parser.add_argument("-rs", "--global_rigidity_steps", type=int, default=5_000,
+                        help="Number of steps to train the foreground and background "
+                             "models with global rigidity loss. (Default: 5_000)")
+
     parser.add_argument("-c", "--config", type=str,
                         help="Path to a custom config file ('.json') with "
                              "algorithm hyper-parameters. (Optional)")
@@ -34,4 +42,9 @@ if __name__ == "__main__":
         device=torch.device(args.device)
     )
 
-    neural_atlases.train(steps=args.steps, pretrain_steps=args.pretrain_steps)
+    neural_atlases.train(
+        steps=args.steps,
+        pretrain_steps=args.pretrain_steps,
+        bootstrapping_steps=args.bootstrapping_steps,
+        global_rigidity_steps=args.global_rigidity_steps
+    )
